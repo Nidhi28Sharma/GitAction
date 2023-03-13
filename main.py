48ec010bee91e5d030d8bfb40c66e2174d59bc6d
NIDHI SHARMA <nidhisharma07257@gmail.com>
2023-03-13 11:11:55 +0530

    2nd Done

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 print("Hello World")
-# def work():
-#     print("Welcome to CloudEQ")
+def work():
+    print("Welcome to CloudEQ Nidhi Sharma")
 
 
-# print("this is my Github Actions")
-# work()
+print("this is my Github Actions workflow")
+work()
