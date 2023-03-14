55414842860b2e8cedb90c7873cb74c3299f2ff3
NIDHI SHARMA <nidhisharma07257@gmail.com>
2023-03-14 16:23:13 +0530

    another commit

--- Pythonfile/func.py
+++ Pythonfile/func.py
@@ -1,10 +1,12 @@
-import sys
+import sys, math
 
-def add(a,b):
-    return a+b
-
-
-
-a=int(sys.argv[1])
-b=int(sys.argv[2])
-c=print(f'The sum of {a} and {b} is:',add(a,b))
+def prime(a):
+    sqtofA = int(math.sqrt(a))
+    for i in range(1, sqtofA):
+        if a%i == 0:
+            return False
+    
+    return True
+    
+pn = int(sys.argv[1])
+print(prime(pn))
